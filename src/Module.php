99d8c11b9e52b7d46e04886b2818fd1728caf9b1
demirmehmet0 <demirmehmet0@yahoo.com>
2022-02-18 23:25:45 +0300

    Inıt Module

--- src/Module.php
+++ src/Module.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace portalium\template;
+namespace diginova\content;
 
 class Module extends \portalium\base\Module
 {
@@ -8,20 +8,20 @@ class Module extends \portalium\base\Module
         [
             'class' => 'yii\rest\UrlRule',
             'controller' => [
-                'template/default',
+                'content/default',
             ]
         ],
     ];
     
     public static function moduleInit()
     {
-        self::registerTranslation('template','@portalium/template/messages',[
-            'template' => 'template.php',
+        self::registerTranslation('content','@diginova/content/messages',[
+            'content' => 'content.php',
         ]);
     }
 
     public static function t($message, array $params = [])
     {
-        return parent::coreT('template', $message, $params);
+        return parent::coreT('content', $message, $params);
     }
 }
