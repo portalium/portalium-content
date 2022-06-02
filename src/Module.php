d1dbb7dc6d3ab28344ef7aa5dcb5ddfd07daad13
demirmehmet0 <demirmehmet0@yahoo.com>
2022-06-02 14:10:11 +0300

    update content

--- src/Module.php
+++ src/Module.php
@@ -10,9 +10,25 @@ class Module extends \portalium\base\Module
             'class' => 'yii\rest\UrlRule',
             'controller' => [
                 'content/default',
-            ]
+                'content/category',
+            ],
+            'pluralize' => false
         ],
     ];
+
+    public function getMenuItems(){
+        $menuItems = [
+            [
+                [
+                    'type' => 'model',
+                    'class' => 'portalium\content\models\Content',
+                    'route' => '/content/default/view',
+                    'field' => [ 'id' => 'id_content', 'name' => 'title' ],
+                ]
+            ],
+        ];
+        return $menuItems;
+    }
 
     public static function moduleInit()
     {
--- src/controllers/api/CategoryController.php
+++ src/controllers/api/CategoryController.php
@@ -5,10 +5,10 @@ namespace portalium\content\controllers\api;
 use portalium\rest\ActiveController as RestActiveController;
 use portalium\content\models\Content;
 
-class DefaultController extends RestActiveController
+class CategoryController extends RestActiveController
 {
-    public $modelClass = 'portalium\content\models\Content';
-
+    public $modelClass = 'portalium\content\models\Category';
+    
     public function actions()
     {
         $actions = parent::actions();
